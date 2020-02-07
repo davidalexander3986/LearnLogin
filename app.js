b9c43a5e37bb652b3c57cc5da0a9d99860a55f04
davidalexander3986 <youremail@university.edu>
2020-02-07 12:34:36 -0800

    Addedindex page

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('IxD secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+var index = require('./routes/index');
+// Add routes here
+app.get('/', index.view);
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
